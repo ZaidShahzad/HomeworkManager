aac1e05da7fe718e5fb292aaa4c50e7ebd963056
Zaid Shahzad <zaidshahzadbusiness@gmail.com>
2023-11-08 20:48:14 -0500

    Going back to original

--- HomeworkManager/main.cpp
+++ HomeworkManager/main.cpp
@@ -1,10 +1,6 @@
 #include <iostream>
 
-int add(int number1, int number2) {
-    return number1 + number2;
-}
-
 int main() {
-    std::cout << "The Sum of 1 + 2 = " << add(1,2) << std::endl;
+    std::cout << "Hello World!" << std::endl;
     return 0;
 }
