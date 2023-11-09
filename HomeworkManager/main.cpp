2467fb361e88289a79fb4c6eb9250339592634c9
Zaid Shahzad <zaidshahzadbusiness@gmail.com>
2023-11-08 20:42:22 -0500

    Added a add function that adds two numbers together and returns their sum

--- HomeworkManager/main.cpp
+++ HomeworkManager/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
 
+int add(int number1, int number2) {
+    return number1 + number2;
+}
+
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "The Sum of 1 + 2 = " << add(1,2) << std::endl;
     return 0;
 }
