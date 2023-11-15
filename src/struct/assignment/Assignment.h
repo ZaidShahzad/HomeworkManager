aa9852311b19d1695665c8a312d07e359f5cb34c
Diegotyb <diego_romero@uri.edu>
2023-11-15 17:10:24 -0500

    Comments for assignment header file

--- src/struct/assignment/Assignment.h
+++ src/struct/assignment/Assignment.h
@@ -8,32 +8,44 @@
 
 
 class Assignment {
+//Private member variables
 private: 
-   std::string title;
-   bool completed;
+   std::string title;                 
+   bool completed;                    
    std::string description;
    int priorityLevel;
    std::string preparedDescription;
 
 public:
+//Public member functions for accessing and modifying private members
+
+//Getters for the title
    std::string getTitle();
 
+//setter for the title
    void setTitle(std::string newTitle);
 
+//getter for the completion status
    bool isCompleted();
 
+//setter for the completion status
    void setCompleted(bool isCompleted);
 
+//getter for the description
    std::string getDescription();
 
+//setter for the description
    void setDescription(std::string newDescription);
 
+//getter for the priority level
    int getPriorityLevel();
 
+//getter for the prepared description
    std::string getPreparationDescription();
 
+//setter for the prepared description
    void setPreparationDescription(std::string newPrepDescription);
 
 };
 
-#endif
+#endif 
