8ade04695375ddde17b001cead2ed1da867f2551
essamabd03 <98502084+essamabd03@users.noreply.github.com>
2023-12-04 13:24:54 -0500

    Update HWMTerminal.cpp
    changed  '-' to '*' in todolist bullet point

--- src/struct/terminal/HWMTerminal.cpp
+++ src/struct/terminal/HWMTerminal.cpp
@@ -382,7 +382,7 @@ void HWMTerminal::gotoAutoTodoList() {
     // Displaying the sorted assignments (Ordered by Priority)
     std::cout << termcolor::yellow << termcolor::bold << "Assignments to do (Ordered by Priority):" << std::endl;
     for (const auto& pair : filteredAssignments) {
-        std::cout << termcolor::yellow << termcolor::bold << " - " << pair.first->getTitle() << std::endl;
+        std::cout << termcolor::yellow << termcolor::bold << " * " << pair.first->getTitle() << std::endl;
     }
 }
 
